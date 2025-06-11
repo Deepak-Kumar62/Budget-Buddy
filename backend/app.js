462b824af83882c91b8fa6bda5b3bac6426caa43
Deepak_Kumar <ydeepak62076@gmail.com>
2025-06-11 16:15:17 +0530

    fix cors origin error

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://monitytrack.netlify.app/",
+    origin: "https://monitytrack.netlify.app",
     credentials: true,
   })
 );
